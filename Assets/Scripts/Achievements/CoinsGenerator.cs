9e8a7505d9c5b520d56acbf67634269850dc3b6c
Pavel ol <megapiharr2008@gmail.com>
2021-06-08 09:11:30 +0300

    removing duplicates from generators, creating parent class

--- Assets/Scripts/Achievements/CoinsGenerator.cs
+++ Assets/Scripts/Achievements/CoinsGenerator.cs
@@ -3,58 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(GameObjectsPool))]
-public class CoinsGenerator : MonoBehaviour
+public class CoinsGenerator : ObjectsGenerator
 {
-    [SerializeField] private float _secondsBetweenSpawn;
     [SerializeField] private Range _yPosRange;
     [SerializeField] private Range _coinsRange;
-    private float _elapsedTime = 0;
-    private GameObjectsPool _objectPools;
 
-    private void Awake()
-    {
-        _objectPools = GetComponent<GameObjectsPool>();
-        _objectPools.Initialize();
-    }
-
-    private void Update()
-    {
-        _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime >= _secondsBetweenSpawn)
-        {
-            GenerateCoinsLine();
-            _elapsedTime = 0;
-        }
-    }
-
-    private void GenerateCoinsLine()
+    protected override void Generate()
     {
         int numbersInLine = (int)_coinsRange.Next();
         float spawnPositionY = _yPosRange.Next();
         float currentXPosition = transform.position.x;
-
+  
         for (int i = 0; i < numbersInLine; i++)
         {
-            if (_objectPools.TryGetObject(out GameObject spawnedObject))
+            Vector3 spawnPosition = new Vector3(currentXPosition, spawnPositionY, transform.position.z);
+            if (IsObjectAvailable(out GameObject spawnedObject))
             {
-                SpawnObject(spawnedObject, currentXPosition, spawnPositionY);
+                SpawnObject(spawnedObject, spawnPosition);
                 currentXPosition += 0.9f;
             }
         }
-    }
-
-    private void SpawnObject(GameObject spawnedObject, float currentXPosition, float spawnPositionY)
-    {
-        Vector3 spawnPosition = new Vector3(currentXPosition, spawnPositionY, transform.position.z);
-        spawnedObject.SetActive(true);
-        spawnedObject.transform.position = spawnPosition;
-
-        _objectPools.DisableObjectAbroadTheScreen();
-    }
-
-    public void Reset()
-    {
-        _objectPools.ResetPool();
     }
 }
--- Assets/Scripts/Obstacles/ObstacleGenerator.cs
+++ Assets/Scripts/Obstacles/ObstacleGenerator.cs
@@ -3,47 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(GameObjectsPool))]
-public class ObstacleGenerator : MonoBehaviour
+public class ObstacleGenerator : ObjectsGenerator
 {
-    [SerializeField] private float _secondsBetweenSpawn;
     [SerializeField] private float _maxSpawnPosition;
     [SerializeField] private float _minSpawnPosition;
 
-    private GameObjectsPool _objectPools;
-    private float _elapsedTime = 0;
-
-    private void Awake()
-    {
-        _objectPools = GetComponent<GameObjectsPool>();
-        _objectPools.Initialize();
-    }
-
-    private void Update()
-    {
-        _elapsedTime += Time.deltaTime;
-
-        if(_elapsedTime >= _secondsBetweenSpawn)
-        {
-            if(_objectPools.TryGetObject(out GameObject spawnedObject))
-            {
-                _elapsedTime = 0;
-                SpawnObject(spawnedObject);
-            }
-        }
-    }
-
-    private void SpawnObject(GameObject spawnedObject)
+    protected override void Generate()
     {
         float spawnPositionY = Random.Range(_minSpawnPosition, _maxSpawnPosition);
         Vector3 spawnPosition = new Vector3(transform.position.x, spawnPositionY, transform.position.z);
-        spawnedObject.SetActive(true);
-        spawnedObject.transform.position = spawnPosition;
-
-        _objectPools.DisableObjectAbroadTheScreen();
-    }
-
-    public void Reset()
-    {
-        _objectPools.ResetPool();
+        if (IsObjectAvailable(out GameObject spawnedObject))
+        {
+            SpawnObject(spawnedObject, spawnPosition);
+        }
     }
 }
